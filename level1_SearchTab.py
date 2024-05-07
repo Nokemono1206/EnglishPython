9c37f8ac0042d5adc2e059eea29e9a0992c2b803
s1300079 <136040179+s1300079@users.noreply.github.com>
2024-05-07 16:54:42 +0900

    level1_2

---
+++ level1_SearchTab.py
@@ -0,0 +1,22 @@
+#coding: utf-8
+
+#import
+
+FileName = input("Please input the file name to check(only .txt file!): ")
+print("The text file is " + '"' + FileName + '"')
+
+SearchingWord = input("Please input the searching word: ")
+print("The searching word is " + '"' + SearchingWord + '"')
+
+FilePath = '/Network/Servers/stdfsv1/vol/vol7/home3/s1300079/AAuP/' + FileName
+print(FilePath)
+
+f = open(FilePath)
+
+fr = f.read()
+
+if fr.find(SearchingWord) != -1:
+    print("Found " + '"' + SearchingWord + '"' + " !")
+else:
+    print("Not found " + '"' + SearchingWord + '"' + " !")
+
