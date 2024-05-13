6d3535d81f65dabc734d7ae37bdf2323eddeabdb
s1300084 <136408789+s1300084@users.noreply.github.com>
2024-05-13 16:51:01 +0900

    Update level1_SearchTab.py

--- level1_SearchTab.py
+++ level1_SearchTab.py
@@ -2,8 +2,11 @@
 
 #import
 
-FileName = input("Please input the file name to check(only .txt file!): ")
-print("The text file is " + '"' + FileName + '"')
+try:
+    FileName = input("Please input the file name to check (only .txt file!): ")
+    print("The text file is " + '"' + FileName + '"')
+except KeyboardInterrupt:
+    print("\nCtrl+C detected. Program terminated.")
 
 SearchingWord = input("Please input the searching word: ")
 print("The searching word is " + '"' + SearchingWord + '"')
